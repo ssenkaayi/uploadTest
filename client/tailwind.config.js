18d6f6880c982a9c8fd0fea87f830efb7321c95e
ssenkaayi <ssenkaaayi@gmail.com>
2024-02-14 12:02:55 +0300

    updating tailwind file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,67 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'regal-violet': 'rgb(113, 99, 186, 255)',
+        'menu':'#e0e0e058',
+        'menu-color':'#fff',
+        'base':'#ebe9e9',
+        "dashbord":"rgba(113,99,186,255)",
+        "search-bar":"rgba(237, 237,237)",
+        "card":"rgba(229,223,223)",
+        
+      
+      },
+      width:{
+        'sidebar':'110px',
+        'hover':'240px',
+        "card-w":"290px"
+       
+      },
+      height:{
+        'sidebar':'100vh',
+        'logo':'80px',
+        'menu':'88%',
+        "card-h":"150px",
+       
+      },
+      padding:{
+        'sidebar-padding':'0 1.7rem',
+        'logo':'16px',
+        'header':'10px 2rem',
+        'primary':['0'],
+        'search-box':'4px 14px',
+        "record":"2rem",
+        "tr":"15px"
+      },
+      transitionProperty:{
+        'sidebar':'transition-all 0.5s linear',
+        'li':'transition-all 0.5s ease-in-out'
+      },
+      margin:{
+        'li':'8px 0',
+        'primary':['0'],
+        "record":"1rem"
+
+      },
+
+      color:{
+        'menu-color':'#fff'
+        
+      },
+      fontFamily: {
+        'primary':['Poppins']
+        
+      },
+      gap:{
+        'body':'1rem',
+        "card":"8px"
+      }
+      
+  
+    },
+
   },
   plugins: [],
 }
