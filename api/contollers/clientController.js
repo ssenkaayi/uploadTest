3cb3e2ece3a534d1f13b36e28a15188394541e40
ssenkaayi <ssenkaaayi@gmail.com>
2024-02-23 13:03:18 +0300

    complete commenting create client function

--- api/contollers/clientController.js
+++ api/contollers/clientController.js
@@ -12,19 +12,31 @@ export const createClient = asyncHandler(async(req,res,next)=>{
   
     // if(error) return next(errorHandler(400,error.details[0].message))
 
+
+    //checking if the supplier exists with the supplier_id in the client table
+
     const supplier = await Supplier.findById(req.body.supplier_id)
     if(!supplier) return res.status(400).send('supplier doesnt exist')
+
+    //assigning supplier_id and trip_id so as to be able to update the weight field
     
     const supplier_id = supplier._id
 
     const trip_id = supplier.trip_id
+
+    //creating new client
     
     const create_client = await Client.create({...req.body,supplier_name:supplier.name})
    
+    //updating supplier number of clients
 
     const updatedSupplier = await Supplier.findByIdAndUpdate({_id:supplier_id},{$push:{client_name:create_client.name,
     client_id:create_client.id,}},{new:true})
-   
+
+
+
+   //updating supplier weight
+
     const new_number_client = (updatedSupplier.client_name)
    
     const new_supplier_weight = updatedSupplier.weight + create_client.weight
@@ -32,19 +44,16 @@ export const createClient = asyncHandler(async(req,res,next)=>{
     const updatedSupplierWeight = await Supplier.findByIdAndUpdate({_id:supplier_id},{$set:{number_clients:new_number_client.length,
         weight:new_supplier_weight,}},{new:true})
 
-    // const supplierWeight = await Supplier.findById(req.body.supplier_id)
-    // if(!supplierWeight) return next(errorHandler(400,'supplier doesnt exist'))
-    // const id = supplierWeight.trip_id
-    // // console.log(supplierWeight)
+
+    //updating trip weight
 
     const trip_weight = await Trip.findById(trip_id)
     const updated_trip_weight = (trip_weight.weight + create_client.weight)
 
-
     await Trip.findByIdAndUpdate({_id:trip_id},{$set:{weight:updated_trip_weight}},{new:true})
     // console.log(updatedTripWeight)
 
-
+    //sending response to the client
     res.status(200).send(create_client)
    
 })
