c78a1e739a20522f79492c30c2d8a48872ca1ce1
ssenkaayi <ssenkaaayi@gmail.com>
2024-03-15 17:52:54 +0300

    adding edit client functionality

--- client/src/Model/EditClient.jsx
+++ client/src/Model/EditClient.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function EditClient({visible , onClose, client_id}) {
 
-  
+  if(!visible) return null;
 
   // console.log(client_id)
 
   const handleOnClose = ()=> onClose()
 
   
-  const[formData,setFormData]=useState({});
-  const[loading,setLoading]=useState(false);
-  const[error,setError]= useState(null);
+  const [formData,setFormData]=useState({});
+  const [loading,setLoading]=useState(false);
+  const [error,setError]= useState(null);
+  const [supplierName,setSupplierName] = useState('')
   
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const handleChange = (e)=>{
     setFormData({
@@ -24,7 +26,46 @@ function EditClient({visible , onClose, client_id}) {
     });
   };
 
-  if(!visible) return null;
+  useEffect(()=>{
+
+
+    fetchClients()
+
+  },[client_id])
+
+  const fetchClients = async()=>{
+
+    try{
+  
+      setLoading(true);
+      const res = await fetch(`/api/client/getClient/${client_id}`,{
+        
+        method:'GET',
+      
+      })
+  
+      const data = await res.json();
+    
+      if(data.succuss===false){
+        setError(true)
+        setLoading(false)
+        return
+      }
+      
+      setError(false)
+      setLoading(false)
+      // console.log(data)
+      setFormData(data)
+      setSupplierName(data.supplier.name)
+    }
+    catch(error){
+      setError(error.message)
+      setLoading(false)
+  
+    }
+  }
+
+  
   
   // max-w-lg mx-auto 
 
@@ -47,28 +88,28 @@ function EditClient({visible , onClose, client_id}) {
                 <div className='flex flex-col gap-4 w-80'>
 
                   <label className='text-1xl font-semibold'>name</label>
-                  <input type="text" placeholder="Enter Password" id='name' 
+                  <input type="text" placeholder="Enter Password" id='name' value={formData.name} 
                   className='border p-3 rounded-lg' required onChange={handleChange}
                   />
 
 
-                  <label className='text-1xl font-semibold'>email</label>
-                  <input type="text" placeholder="Enter Password" id='email' 
+                  <label className='text-1xl font-semibold'>weight</label>
+                  <input type="text" placeholder="Enter Password" id='weight' value={formData.weight} 
                   className='border p-3 rounded-lg' required onChange={handleChange}
                   />
 
 
-                  <label className='text-1xl font-semibold'> address </label>
-                  <input type="text" placeholder="Enter Password" id='address' 
+                  <label className='text-1xl font-semibold'> supplier</label>
+                  <input type="text" placeholder="Enter Password" id='supplier' value={supplierName}  
                   className='border p-3 rounded-lg' required onChange={handleChange}
                   />
 
 
                 </div>
 
-                <div className='flex flex-col gap-4 w-80'>
+                {/* <div className='flex flex-col gap-4 w-80'>
 
-                  <label className='text-1xl font-semibold'>Phone</label>
+                  <label className='text-1xl font-semibold'>date</label>
                   <input type="text" placeholder="Enter Password" id='phone' 
                   className='border p-3 rounded-lg' required onChange={handleChange}
                   />
@@ -85,7 +126,7 @@ function EditClient({visible , onClose, client_id}) {
                   className='border p-3 rounded-lg' required onChange={handleChange}
                   />
 
-                </div>
+                </div> */}
 
               </div>
 
