adbd4bf0428f3b7a0fe6a73dee25c707c3380ed2
ssenkaayi <ssenkaaayi@gmail.com>
2024-04-05 16:55:15 +0300

    correcting error handler in the trip controller

--- api/contollers/tripController.js
+++ api/contollers/tripController.js
@@ -1,13 +1,15 @@
 import { tripValidation  } from '../validation.js'
 import asyncHandler from 'express-async-handler'
 import Trip from '../model/tripModel.js';
+import errorHandler from '../errorHandler.js';
 
 
 
 export const createTrip = asyncHandler(async(req,res)=>{
 
     const {error} = tripValidation(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    if(error) return next(errorHandler(400,error.details[0].message))
+    // res.status(400).send(error.details[0].message)
 
     const createTrip = await Trip.create(req.body)
     res.status(200).send(createTrip)
